783fe54c7f315904c873301752dbf8d7b743db5c
yalin-yoshva <65439755+yalin-yoshva@users.noreply.github.com>
2021-11-25 20:11:28 +0530

    Update system.h

--- 3_Implementation/inc/system.h
+++ 3_Implementation/inc/system.h
@@ -1,14 +1,4 @@
 #include<stdio.h>
+ #include<conio.h>
  #include<string.h>
  #include<ctype.h>
- struct Medicine{
-   int id,price,quantity;
-   char medicneName[100],Company[100],Mfg_Date[11],Exp_Date[11],info[5000];
-  }m[100];
- void PurchaseMedicine(int number);
- void EnterInfoAboutMedicine(int number);
- void StockOfMedicine(int number);
- void KnowInfoAboutMedicine(int number);
- void AddMedicineinStore(int number,struct Medicine m[]);
- void DeleteMedicineStore(int number);
- void ChangeMedicineDetails(int number);
