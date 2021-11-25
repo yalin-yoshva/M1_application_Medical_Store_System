b0a41b9b5e931b8b83950d8652e84515374d79ee
yalin-yoshva <65439755+yalin-yoshva@users.noreply.github.com>
2021-11-25 20:23:59 +0530

    Update system.h

--- 3_Implementation/inc/system.h
+++ 3_Implementation/inc/system.h
@@ -1,4 +1,8 @@
 #include<stdio.h>
- #include<conio.h>
- #include<string.h>
- #include<ctype.h>
+#include<conio.h>
+#include<string.h>
+#include<math.h>
+#include<dos.h>
+#include<time.h>
+#include<ctype.h>
+#include<windows.h>
